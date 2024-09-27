775cbfcb3e954e6582c88580cb9b44c0c40222c1
C S AMRITHA <116827842+CS-Amritha@users.noreply.github.com>
2024-09-27 07:37:57 +0530

    Create 1_Max_Min_in_Array.py

---
+++ 1_Max_Min_in_Array.py
@@ -0,0 +1,21 @@
+def findMaxMin(arr):    
+    max_val = -float('inf')
+    min_val = float('inf')
+    
+    for i in arr:
+        if i > max_val:
+            max_val = i
+            
+        if i < min_val:
+            min_val = i
+            
+    print("max:",max_val)
+    print("min:",min_val)    
+    
+arr1 = [3,5,4,1,9]
+arr_input = int(input("Enter the values separated by spaces:"))
+arr = list(map(int,arr_input.split()))
+#arr_input.split()   Results in: ['3', '5', '4', '1', '9']
+#map(int, ['3', '5', '4', '1', '9']) Results in: [3, 5, 4, 1, 9]
+#The map() function returns an iterable (in this case, a map object), so you need to convert it into a list.
+findMaxMin(arr)
