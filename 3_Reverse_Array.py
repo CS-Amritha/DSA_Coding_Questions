b74f7a4e9b86a933df47de4cc1c04fa145155760
C S AMRITHA <116827842+CS-Amritha@users.noreply.github.com>
2024-09-29 11:19:40 +0530

    Create 3_Reverse_Array.py
    Reverse an array

---
+++ 3_Reverse_Array.py
@@ -0,0 +1,25 @@
+'''def reverse(arr):
+    return arr[::-1]
+arr = [1,2,3,4,5]
+print(reverse(arr))'''
+
+def reverse(arr):
+    
+    n = len(arr)
+    temp = [0]* n
+    
+    #copy to temp in reverse order
+    for i in range(n):
+        temp[i] = arr[n - i - 1]
+        
+    #overwrite temp to arr
+    for i in range(n):
+        arr[i] = temp[i]
+        
+    return arr
+        
+arrinput = input("Enter array values separted with space:")
+
+arr = list(map(int,arrinput.split()))
+
+print(reverse(arr))
