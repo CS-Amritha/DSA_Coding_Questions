94f3726e9554ff1fabaf66051bf9150edd744019
C S AMRITHA <116827842+CS-Amritha@users.noreply.github.com>
2024-11-21 19:46:51 +0530

    Create 6_Depth_First_Search.py

---
+++ 6_Depth_First_Search.py
@@ -0,0 +1,22 @@
+
+
+from typing import List
+
+class Solution:
+    def dfsOfGraph(self, adj: List[List[int]]) -> List[int]:
+        def dfs(node):
+
+            visited.add(node)
+            dfs_traversal.append(node)
+            
+            
+            for neighbor in adj[node]:
+                if neighbor not in visited:
+                    dfs(neighbor)
+
+        visited = set()  
+        dfs_traversal = []  
+        
+        dfs(0)
+        
+        return dfs_traversal
