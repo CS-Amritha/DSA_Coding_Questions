d435a240004d99d95b05b36c58e77620264fdb85
C S AMRITHA <116827842+CS-Amritha@users.noreply.github.com>
2024-11-25 09:50:14 +0530

    ReversedLinkedList

---
+++ 7_ReversedLinkedList.py
@@ -0,0 +1,16 @@
+class Node(object):
+    def __init__(self,val):
+        self.val = val
+        self.head = None 
+
+class Solution(object):
+    def reverseList(self,head):
+        prev = None
+        curr = head
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+
+        return prev
