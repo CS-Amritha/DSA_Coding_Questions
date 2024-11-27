5c7bf49e334cb9952ca36c8133ea76118e0616c5
C S AMRITHA <116827842+CS-Amritha@users.noreply.github.com>
2024-11-27 11:43:09 +0530

    RansomNote
    https://leetcode.com/problems/ransom-note/submissions/1463962878

---
+++ 12_RansomNote.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        countR = {}
+        countM = {}
+
+
+        for i in range(len(magazine)):
+            countM[magazine[i]] = 1 + countM.get(magazine[i], 0)
+
+
+        for i in range(len(ransomNote)):
+            countR[ransomNote[i]] = 1 + countR.get(ransomNote[i], 0)
+
+
+        for char in countR:
+            if countR[char] > countM.get(char, 0):
+                return False
+
+        return True
